b957bda1d8f79c431f0afa410de6b113fe4ef7f2
stan <stanislav@synopsys.com>
2020-06-25 20:23:40 +0200

    aa

--- vars/checkout.groovy
+++ vars/checkout.groovy
@@ -1,3 +1,3 @@
-def checkOut(repo, branch){
-git branch: "${branch}",url:"${repo}"
+def checkOut(repo, branch,credentials){
+git branch: "${branch}",url:"${repo}", credentialsId: "${credentials}"
 }
